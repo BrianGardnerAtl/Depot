5d0410fae06866910594b961ded4e406df525ead
= <=>
2011-05-12 16:38:18 -0400

    depot scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'dcce3578fc7e8cf0ceadfb23bdceb54a887fafed67c32181ac646f194ce545c49266aa69aa346fde9b0ff2e04194d62d531c31400ef331bb1936684b6cae0855'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+
+Product.create(:title => 'Programming Ruby 1.9',
+              :description =>
+                %{<p>
+                  Ruby is an awesome dynamic language.
+                  </p>},
+              :image_url => '/images/ruby.jpg',
+              :price => 49.50)
