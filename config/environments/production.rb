04dd770b8784cbb91104eda71faa9bbecd076425
= <=>
2011-05-17 12:10:48 -0400

    added pagination to orders

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,18 @@ Depot::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Example mail settings for Gmail
+  # config.action_mailer.delivery_method = :smtp
+  # 
+#   config.action_mailer.smtp_settings = {
+#     :address => "smtp.gmail.com",
+#     :port => 587,
+#     :domain => "domain.of.sender.net",
+#     :authentication => "plain",
+#     :user_name => "dave",
+#     :password => "secret",
+#     :enable_starttls_auto => true
+#   }
+
 end
